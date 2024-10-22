5cfe076e31beed864990b2664a54339106793ee4
MKsenni <pen.cil@mail.ru>
2024-10-22 22:48:11 +0500

    fix: add import process from node

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
 import path from 'path';
 import { dirname } from 'node:path';
+import process from 'node:process';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-
 export default {
-    mode: process.env.NODE_ENV || 'production',
-    entry: './src/index.ts',
-    target: 'node',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        chunkFormat: 'module',
-        library: {
-            type: 'module',
-        }
-    },
-    experiments: {
-        outputModule: true,
-    },
+  mode: process.env.NODE_ENV || 'production',
+  entry: './src/index.ts',
+  target: 'node',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    chunkFormat: 'module',
+    library: {
+      type: 'module'
+    }
+  },
+  experiments: {
+    outputModule: true
+  }
 };
